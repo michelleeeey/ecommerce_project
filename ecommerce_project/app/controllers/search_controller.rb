fb339ecd7841a6da9f9cade937af09b92690511e
Michelle <michelle.aldn@outlook.com>
2019-03-22 12:42:23 -0500

    Implemented the result action in the search controller.

--- ecommerce_project/app/controllers/search_controller.rb
+++ ecommerce_project/app/controllers/search_controller.rb
@@ -1,4 +1,33 @@
+# frozen_string_literal: true
+
 class SearchController < ApplicationController
   def results
+    @query = params[:q]
+    @search_option = params[:search_option]
+
+    if @query == ''
+      @search_query = []
+    else
+      case @search_option
+      when 'Search All'
+        @search_query = Product.where('name LIKE ?', "%#{@query}%")
+
+      when 'Monitors'
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Monitors')).where('name LIKE ?', "%#{@query}%")
+
+      when 'Keyboard'
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Keyboard')).where('name LIKE ?', "%#{@query}%")
+
+      when 'Mouse'
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Mouse')).where('name LIKE ?', "%#{@query}%")
+
+      when 'Printer'
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Printer')).where('name LIKE ?', "%#{@query}%")
+
+      when 'Webcam'
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Webcam')).where('name LIKE ?', "%#{@query}%")
+    end
+
+    end
   end
 end
