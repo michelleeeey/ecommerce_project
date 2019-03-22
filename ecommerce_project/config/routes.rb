fbb6797afcf617f20fc1dba2b2d7c6f728debc11
Michelle <michelle.aldn@outlook.com>
2019-03-21 22:26:43 -0500

    Added the Products controller.

--- ecommerce_project/config/routes.rb
+++ ecommerce_project/config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources 'products', only: %i[index show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
