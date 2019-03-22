a240bd26b9f4277ec7ce8e829bc6074c066bcbc8
Michelle <michelle.aldn@outlook.com>
2019-03-22 12:39:21 -0500

    Added a self paging method to the product model.

--- ecommerce_project/app/controllers/products_controller.rb
+++ ecommerce_project/app/controllers/products_controller.rb
@@ -3,11 +3,10 @@
 class ProductsController < ApplicationController
   def index
     page = params[:page]
-
     @products = if params[:category_type]
-                  Product.where("category_id = #{params[:category_type]}")
+                  Product.where("category_id = #{params[:category_type]}").paging(page)
                 else
-                  Product.order(:name)
+                  Product.order(:name).paging(page)
                 end
   end
 
