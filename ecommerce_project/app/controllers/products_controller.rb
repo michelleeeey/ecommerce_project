308d18786c8c8567f9d395ffc5b2e6b1cdcb1875
Michelle <michelle.aldn@outlook.com>
2019-03-22 00:42:50 -0500

    Implemented the products controller index and show action.

--- ecommerce_project/app/controllers/products_controller.rb
+++ ecommerce_project/app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
   def index
+    page = params[:page]
+
+    @products = if params[:category_type]
+                  Product.where("category_id = #{params[:category_type]}")
+                else
+                  Product.order(:name)
+                end
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
