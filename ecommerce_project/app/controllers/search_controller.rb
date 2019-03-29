ce2a18ef78b25f465c6ea48ef0705219646bbd67
Michelle <michelle.aldn@outlook.com>
2019-03-28 19:38:42 -0500

    Search through the available products using a keyword search by category.

--- ecommerce_project/app/controllers/search_controller.rb
+++ ecommerce_project/app/controllers/search_controller.rb
@@ -10,22 +10,22 @@ class SearchController < ApplicationController
     else
       case @search_option
       when 'Search All'
-        @search_query = Product.where('name LIKE ?', "%#{@query}%")
+        @search_query = Product.where('product_name LIKE ?', "%#{@query}%")
 
-      when 'Monitors'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Monitors')).where('name LIKE ?', "%#{@query}%")
+      when 'Monitor'
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Monitor')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Keyboard'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Keyboard')).where('name LIKE ?', "%#{@query}%")
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Keyboard')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Mouse'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Mouse')).where('name LIKE ?', "%#{@query}%")
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Mouse')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Printer'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Printer')).where('name LIKE ?', "%#{@query}%")
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Printer')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Webcam'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Webcam')).where('name LIKE ?', "%#{@query}%")
+        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Webcam')).where('product_name LIKE ?', "%#{@query}%")
     end
 
     end
