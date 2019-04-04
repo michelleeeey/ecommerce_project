e089fe3f505ada72198851b32d62c01f670eb27a
Michelle <michelle.aldn@outlook.com>
2019-04-03 20:27:24 -0500

    Modified css

--- ecommerce_project/app/controllers/search_controller.rb
+++ ecommerce_project/app/controllers/search_controller.rb
@@ -6,26 +6,26 @@ class SearchController < ApplicationController
     @search_option = params[:search_option]
 
     if @query == ''
-      @search_query = []
+      @products = []
     else
       case @search_option
       when 'Search All'
-        @search_query = Product.where('product_name LIKE ?', "%#{@query}%")
+        @products = Product.where('product_name LIKE ?', "%#{@query}%")
 
       when 'Monitor'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Monitor')).where('product_name LIKE ?', "%#{@query}%")
+        @products = Product.joins(:category).merge(Category.where('name LIKE ?', 'Monitor')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Keyboard'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Keyboard')).where('product_name LIKE ?', "%#{@query}%")
+        @products = Product.joins(:category).merge(Category.where('name LIKE ?', 'Keyboard')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Mouse'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Mouse')).where('product_name LIKE ?', "%#{@query}%")
+        @products = Product.joins(:category).merge(Category.where('name LIKE ?', 'Mouse')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Printer'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Printer')).where('product_name LIKE ?', "%#{@query}%")
+        @products = Product.joins(:category).merge(Category.where('name LIKE ?', 'Printer')).where('product_name LIKE ?', "%#{@query}%")
 
       when 'Webcam'
-        @search_query = Product.joins(:category).merge(Category.where('name LIKE ?', 'Webcam')).where('product_name LIKE ?', "%#{@query}%")
+        @products = Product.joins(:category).merge(Category.where('name LIKE ?', 'Webcam')).where('product_name LIKE ?', "%#{@query}%")
     end
 
     end
